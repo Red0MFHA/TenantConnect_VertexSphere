211a4165b3f6960a4fa8ae8317e4e6dcaaa861a7
Muhammad Fawad Hussain Awan <i230719@isb.nu.edu.pk>
2025-11-22 16:52:40 +0500

    Lgin Signup Linkage Done

--- src/main/java/com/example/tenantconnect/HelloApplication.java
+++ src/main/java/com/example/tenantconnect/HelloApplication.java
@@ -11,10 +11,10 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/tenantconnect/new.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 700);
 
         scene.getStylesheets().add(
-                getClass().getResource("/com/example/tenantconnect/app.css").toExternalForm()
+                HelloApplication.class.getResource("/com/example/tenantconnect/app.css").toExternalForm()
         );
 
         stage.setTitle("Hello!");
